b9c375d0ec2d8cd65ccb717c40de5d978da2f9aa
Joe Zucco <josephzucco@gmail.com>
2022-05-03 15:33:18 -0400

    get a single todo

--- server/index.js
+++ server/index.js
@@ -14,11 +14,23 @@ app.get('/todos', async (req, res) => {
   try {
     const allTodos = await pool.query('SELECT * FROM todo');
     res.json(allTodos.rows);
-  } catch (err) {}
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
-//get a todos
-
+//get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //create a todo
 app.post('/todos', async (req, res) => {
