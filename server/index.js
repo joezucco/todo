e668c1b3a1f521592a02ae194257541e96e83587
Joe Zucco <josephzucco@gmail.com>
2022-05-03 15:16:49 -0400

    create todo, post request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //middleware
 app.use(cors());
-app.use(express.json()) // allows us to access the req.body
+app.use(express.json()); // allows us to access the req.body
+
+//ROUTES//
+
+//get all todos
+
+//get a todo
+
+//create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    res.json(req.body);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
   console.log('server started on port 5000');
