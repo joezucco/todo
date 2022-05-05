309288a0821704a71288641c5cf9f8054ddf6836
Joe Zucco <josephzucco@gmail.com>
2022-05-05 14:11:47 -0400

    db

--- server/index.js
+++ server/index.js
@@ -63,6 +63,17 @@ app.put('/todos/:id', async (req, res) => {
 });
 
 //delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+    res.json('Todo was deleted!');
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log('server started on port 5000');
