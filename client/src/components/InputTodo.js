e669f3c2956a28c6fcddc659f168e01e0df25ae9
Joe Zucco <josephzucco@gmail.com>
2022-05-05 14:58:30 -0400

    build out InputTodo component

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -2,15 +2,32 @@ import { useState } from 'react';
 
 const InputTodo = () => {
   const [description, setDescription] = useState('');
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch('http://localhost:5000/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
     <div>
       <h1 className='text-center my-5'>Input Todo</h1>
-      <form
-        className='d-flex'
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      >
-        <input type='text' placeholder='add todo' className='form-control' />
+      <form className='d-flex' onSubmit={onSubmitForm}>
+        <input
+          type='text'
+          placeholder='add todo'
+          className='form-control'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button className='btn btn-success'>+</button>
       </form>
     </div>
