6b4f723dbb50ea9fa7c6dc216fa0b4262ea1c9cf
Joe Zucco <josephzucco@gmail.com>
2022-05-05 15:14:57 -0400

    list all todos in ListTodos component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className='App'>
       <div className='container'>
         <InputTodo />
+        <ListTodos />
       </div>
     </div>
   );
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,46 @@
+import { useState, useEffect } from 'react';
+
 const ListTodos = () => {
-  return <div>ListTodos</div>;
+  const [todos, setTodos] = useState([]);
+
+  async function getTodos() {
+    const res = await fetch('http://localhost:5000/todos');
+    const todoArray = await res.json();
+    setTodos(todoArray);
+  }
+  useEffect(() => {
+    getTodos();
+  }, []);
+  return (
+    <>
+      <table className='table mt-5'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+
+          {todos.map((todo) => {
+            return (
+              <tr>
+                <td>{todo.description}</td>
+                <td>Edit</td>
+                <td>Delete</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default ListTodos;
