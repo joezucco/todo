3fa78967084c2cebcec3fb4f79fda77f504bf0fa
Joe Zucco <josephzucco@gmail.com>
2022-05-05 14:42:33 -0400

    set state on input todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 
 const InputTodo = () => {
+  const [description, setDescription] = useState('');
   return (
     <div>
-      <h1 className='text-center my-5'>InputTodo</h1>
-      <form className='d-flex'>
+      <h1 className='text-center my-5'>Input Todo</h1>
+      <form
+        className='d-flex'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      >
         <input type='text' placeholder='add todo' className='form-control' />
         <button className='btn btn-success'>+</button>
       </form>
